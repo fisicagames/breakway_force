47fbc64bd570713dbc9f62f8e39a991dcb9c5e45
fisicagames <rafaelj3d@gmail.com>
2024-06-05 11:28:27 -0300

    Update plankAggregate

--- refs/codeRefs.md
+++ refs/codeRefs.md
@@ -1,19 +1,23 @@
 # Havok Physics Basic
 
-https://playground.babylonjs.com/#WXSQIF#2
+[https://playground.babylonjs.com/#WXSQIF#2]()
 
-https://playground.babylonjs.com/#FLCVX1#3
+[https://playground.babylonjs.com/#FLCVX1#3]()
 
-https://playground.babylonjs.com/#RQIZD3#1
+[https://playground.babylonjs.com/#RQIZD3#1]()
 
 # [Unable to load Havok plugin (error while loading .wasm file from browser)](https://forum.babylonjs.com/t/unable-to-load-havok-plugin-error-while-loading-wasm-file-from-browser/40289)
 
 https://github.com/michealparks/babylon-template/blob/main/src/physics.ts
 
-https://forum.babylonjs.com/t/unable-to-load-havok-plugin-error-while-loading-wasm-file-from-browser/40289/44?u=rafaelj
-
-
+[https://forum.babylonjs.com/t/unable-to-load-havok-plugin-error-while-loading-wasm-file-from-browser/40289/44?u=rafaelj]()
 
 # Why WASM is not the future of Babylon.js
 
-https://babylonjs.medium.com/why-wasm-is-not-the-future-of-babylon-js-5832b09c9b10
+[https://babylonjs.medium.com/why-wasm-is-not-the-future-of-babylon-js-5832b09c9b10]()
+
+# Axis:
+
+[https://www.babylonjs-playground.com/#2JKA91]()
+
+![1717537920542](image/codeRefs/1717537920542.png)
--- src/applicatonLayer/ObjectsInitializer.ts
+++ src/applicatonLayer/ObjectsInitializer.ts
@@ -1,4 +1,4 @@
-import { Scene, MeshBuilder, Mesh, Vector3, HavokPlugin, PhysicsAggregate, PhysicsShapeType, PhysicsViewer, KeyboardEventTypes, FollowCamera} from "@babylonjs/core";
+import { Scene, MeshBuilder, Mesh, Vector3, HavokPlugin, PhysicsAggregate, PhysicsShapeType, PhysicsViewer, KeyboardEventTypes, FollowCamera, PhysicsMotionType, StandardMaterial, Color3, Material} from "@babylonjs/core";
 import HavokPhysics from "@babylonjs/havok";
 
 export class ObjectsInitializer {
@@ -28,29 +28,48 @@ export class ObjectsInitializer {
 
         const ground = MeshBuilder.CreateGround("ground", { width: 30, height: 30 }, this._scene);
         const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, this._scene);
-
+        const groundMaterial = new StandardMaterial("groundMaterial", this._scene);
+        groundMaterial.diffuseColor = Color3.Random();
+        ground.material = groundMaterial;
 
         this._plank = MeshBuilder.CreateBox("plank", { size: 2, width: 18, height: 0.5 }, this._scene);
         this._plank.position = new Vector3(0, 2.5, 0);
-        
-        const planeAggregate = new PhysicsAggregate(this._plank, PhysicsShapeType.BOX, { mass: 1 }, this._scene);
+        const plankMaterial = new StandardMaterial("plankMat", this._scene);
+        plankMaterial.diffuseColor = Color3.Random();
+        this._plank.material = plankMaterial;
+
+
+        const plankAggregate = new PhysicsAggregate(this._plank, PhysicsShapeType.BOX, { mass: 1 }, this._scene);
         //planeAggregate.body.applyAngularImpulse(new Vector3(0, 0, 2));
-        planeAggregate.body.disablePreStep = false;
+        plankAggregate.body.disablePreStep = false;
+        plankAggregate.body.setMotionType(PhysicsMotionType.ANIMATED)
         
 
         const box: Mesh = MeshBuilder.CreateBox("box", { size: 1, width: 2, height: 1 }, this._scene);
         box.position = new Vector3(0, 0.5, 0);
         const boxAggregate = new PhysicsAggregate(box, PhysicsShapeType.BOX, { mass: 1 }, this._scene);
+        const boxMaterial = new StandardMaterial("boxMaterial", this._scene);
+        boxMaterial.diffuseColor = Color3.Random();
+        box.material = boxMaterial;
         
         
         const box2: Mesh = MeshBuilder.CreateBox("box2", { size: 1, width: 2, height: 1 }, this._scene);
         //box2.position = new Vector3(0, 5.5, 0);
         const boxAggregate2 = new PhysicsAggregate(box2, PhysicsShapeType.BOX, { mass: 1 }, this._scene);
+        const box2Material = new StandardMaterial("box2Material", this._scene);
+        box2Material.diffuseColor = Color3.Random();
+        box.material = box2Material;
+
         
         const mainBox: Mesh = MeshBuilder.CreateBox("mainBox", { size: 1, width: 1, height: 1 }, this._scene);
         mainBox.position.y = 4;
+        const mainBoxMaterial = new StandardMaterial("mainBoxMaterial", this._scene);
+        mainBoxMaterial.diffuseColor = Color3.Random();
+        mainBox.material = mainBoxMaterial;
+
+
+        
         const boxAggregate3 = new PhysicsAggregate(mainBox, PhysicsShapeType.BOX, { mass: 1 }, this._scene);
-       
         this._camera.lockedTarget = mainBox;
 
         
@@ -60,12 +79,12 @@ export class ObjectsInitializer {
                     switch (kbInfo.event.key) {
                         case "a":
                         case "A":
-                            hk.setAngularVelocity(planeAggregate.body,new Vector3(0,0,-0.5));
+                            hk.setAngularVelocity(plankAggregate.body,new Vector3(0,0,-0.05));
                         break
                         case "d":
                         case "D":
                             this._plank.rotation.y -= this._rotationSpeed;
-                            hk.setAngularVelocity(planeAggregate.body,new Vector3(0,0,0.5));
+                            hk.setAngularVelocity(plankAggregate.body,new Vector3(0,0,0.05));
                         break
                         case "w":
                         case "W":
