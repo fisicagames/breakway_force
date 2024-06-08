b9def7e4b1d8a11ca941fe738f627d28dff37f0d
fisicagames <rafaelj3d@gmail.com>
2024-06-08 08:06:52 -0300

    Refactor ObjectsController

--- src/domain/ObjectsController.ts
+++ src/domain/ObjectsController.ts
@@ -19,56 +19,28 @@ export class ObjectsController {
         this._camera = camera;
         this._physicsEngine = physicsPlugin;
         this._guiControllerInterface = guiController;
-
+        this.initialize();
     }
 
-    public async initialize(): Promise<void> {
-        const hk = this._physicsEngine;
-
+    private initialize() {
         const sky = new Sky(this._scene);
-
-        for (let i = 0; i < 10; i++) {
-            const plank = new Plank(this._scene, new Vector3(24 * i, -2.5 * i + 2.5, 0), { size: 4, width: 24, height: 0.5 }, 1, 0.01);
-            if (i === 0) {
-                this._currentPlank = plank.physicsBody;
-            }
-        }
+        this.createPlanks();
 
         const box = new Box(this._scene);
         this._netForceVectorLine = new NetForceVectorLine(this._scene, box.mesh);
 
         this._camera.lockedTarget = box.mesh;
 
-        this._scene.onKeyboardObservable.add((kbInfo) => {
-            switch (kbInfo.type) {
-                case KeyboardEventTypes.KEYDOWN:
-                    switch (kbInfo.event.key) {
-                        case "a":
-                        case "A":
-                        case "ArrowLeft":
-                            hk.setAngularVelocity(this._currentPlank, new Vector3(0, 0, this._rotationSpeed));
-                            break;
-                        case "d":
-                        case "D":
-                        case "ArrowRight":
-                            hk.setAngularVelocity(this._currentPlank, new Vector3(0, 0, -this._rotationSpeed));
-                            break;
-                    }
-                    break;
-            }
-        });
+        const hk = this._physicsEngine;
+        this.keyboardInput(hk);
 
-        hk.onCollisionObservable.add((ev) => {
-            if (this._currentPlank !== ev.collider) {
-                this._currentPlank = ev.collider;
-            }
-        });
+        this.plankCollisionHandler(hk);
 
-        let i: number = 0;
+        let iFramesCount: number = 0;
         let lastBoxLinearVelocity = box.physicsBody.getLinearVelocity();
 
         this._scene.onBeforeRenderObservable.add(() => {
-            i++;
+            iFramesCount++;
             if (this._guiControllerInterface.buttonLeftIsDown) {
                 hk.setAngularVelocity(this._currentPlank, new Vector3(0, 0, this._rotationSpeed));
 
@@ -77,7 +49,7 @@ export class ObjectsController {
                 hk.setAngularVelocity(this._currentPlank, new Vector3(0, 0, -this._rotationSpeed));
 
             }
-            if (i % 5 === 0) {
+            if (iFramesCount % 5 === 0) {
                 const newBoxLinearVelocity = box.physicsBody.getLinearVelocity();
                 const accelerationBox = newBoxLinearVelocity.subtract(lastBoxLinearVelocity);
                 lastBoxLinearVelocity = newBoxLinearVelocity;
@@ -92,5 +64,46 @@ export class ObjectsController {
                 //to do: code for restart all objects!
             }
         });
+    }
+
+    private createPlanks() {
+        for (let i = 0; i < 10; i++) {
+            const plank = new Plank(this._scene, new Vector3(24 * i, -2.5 * i + 2.5, 0), { size: 4, width: 24, height: 0.5 }, 1, 0.01);
+            if (i === 0) {
+                this._currentPlank = plank.physicsBody;
+            }
+        }
+    }
+    private plankCollisionHandler(hk: HavokPlugin) {
+        hk.onCollisionObservable.add((ev) => {
+            if (this._currentPlank !== ev.collider) {
+                this._currentPlank = ev.collider;
+            }
+        });
+    }
+
+    private keyboardInput(hk: HavokPlugin) {
+        this._scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.type) {
+                case KeyboardEventTypes.KEYDOWN:
+                    switch (kbInfo.event.key) {
+                        case "a":
+                        case "A":
+                        case "ArrowLeft":
+                            this.rotatePlank(1);
+                            break;
+                        case "d":
+                        case "D":
+                        case "ArrowRight":
+                            this.rotatePlank(-1);
+                            break;
+                    }
+                    break;
+            }
+        });
+    }
+
+    public rotatePlank(factor:number) {
+        this._physicsEngine.setAngularVelocity(this._currentPlank, new Vector3(0, 0, factor*this._rotationSpeed));
     }
 }
